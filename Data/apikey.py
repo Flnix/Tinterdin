d4cc13f54867e1ab2c63b306ab0166b34d490ff1
Deepan V <vdeepan272@proton.me>
2023-01-06 20:16:50 +0530

    Update apikey.py

--- Data/apikey.py
+++ Data/apikey.py
@@ -1,4 +1,4 @@
-openai_apikey="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"
-email="vdeepan272@gmail.com"
-email_password="Doraemom@007"
-wolframalpha_id ='EGQ9WY-8W3XWQ28KH'
+openai_apikey="<your-api>"
+email="<your email>"
+email_password="your email password"
+wolframalpha_id ="<your-api>"
